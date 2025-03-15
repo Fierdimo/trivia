992a19fdd1f889c1f718bb606d06ef48c801c1fb
fierdimo <grmoralesp@gmail.com>
2025-03-14 20:38:01 -0500

    added default admin credentials

--- trivia-backend/src/main.ts
+++ trivia-backend/src/main.ts
@@ -15,7 +15,7 @@ async function bootstrap() {
   SwaggerModule.setup('api', app, document);
 
   app.enableCors({
-    origin: 'http://localhost:3001',
+    origin: process.env.FRONT_URL,
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
   });
--- trivia-backend/src/users/users.module.ts
+++ trivia-backend/src/users/users.module.ts
@@ -1,8 +1,11 @@
-import { Module } from '@nestjs/common';
+import { Module, OnModuleInit } from '@nestjs/common';
 import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { User } from './entities/user.entity';
+import { User, UserRole } from './entities/user.entity';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import * as bcrypt from 'bcryptjs';
 
 @Module({
   controllers: [UsersController],
@@ -10,4 +13,36 @@ import { User } from './entities/user.entity';
   imports: [TypeOrmModule.forFeature([User])],
   exports: [UsersService],
 })
-export class UsersModule {}
+export class UsersModule implements OnModuleInit {
+  constructor(
+    @InjectRepository(User)
+    private readonly userRepository: Repository<User>,
+    private readonly usersService: UsersService,
+  ) {}
+
+  async onModuleInit() {
+    const email = process.env.ADMIN_EMAIL;
+    const password = process.env.ADMIN_PASSWORD;
+
+    if (!email || !password) {
+      throw new Error(
+        'Admin email and password must be set in environment variables',
+      );
+    }
+
+    const adminExists = await this.userRepository.findOne({
+      where: { email, role: UserRole.ADMIN },
+    });
+
+    if (!adminExists) {
+      this.usersService.create({
+        password,
+        email,
+        role: UserRole.ADMIN,
+      });
+      console.log('Admin user created successfully');
+    } else {
+      console.log('Admin user already exists');
+    }
+  }
+}
